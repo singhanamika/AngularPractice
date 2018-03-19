313be26baf46b2d3bf8c5de798aa162683d0852d
singhanamika <anasingh1593@gmail.com>
2018-03-18 22:37:33 -0400

    Create mock-courses.ts

---
+++ src/app/mock-courses.ts
@@ -0,0 +1,14 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  { id: 11, name: 'Mr. Nice', age: 15, subject: 'Science' },
+  { id: 12, name: 'Narco', age: 15, subject: 'Science'  },
+  { id: 13, name: 'Bombasto', age: 15, subject: 'Science'  },
+  { id: 14, name: 'Celeritas' , age: 15, subject: 'Science' },
+  { id: 15, name: 'Magneta', age: 15, subject: 'Science'  },
+  { id: 16, name: 'RubberMan', age: 15, subject: 'Science'  },
+  { id: 17, name: 'Dynama', age: 15, subject: 'Science'  },
+  { id: 18, name: 'Dr IQ', age: 15, subject: 'Science'  },
+  { id: 19, name: 'Magma', age: 15, subject: 'Science'  },
+  { id: 20, name: 'Tornado', age: 15, subject: 'Science'  }
+];

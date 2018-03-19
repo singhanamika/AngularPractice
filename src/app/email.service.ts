67f647bc6199903357d97ef9004a86e5009b7716
singhanamika <anasingh1593@gmail.com>
2018-03-18 22:38:52 -0400

    Second Commit

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,21 +1,14 @@
-
+import { Course } from './course';
+import { COURSES } from './mock-courses';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class EmailService {
-	
-	getCourses(){
-		
-		return [{id: '1', name: 'course1'},
-		{id: '12', name: 'course4'},
-		{id: '13', name: 'course5'},
-		{id: '100', name: 'course100'},
-		{id: '2', name: 'course2'},
-		{id: '3', name: 'course3'}
-		];
-		
-	}
-
+getCourses(): Observable<Course[]> {
+  return of(COURSES);
+}
   constructor() { }
 
 }

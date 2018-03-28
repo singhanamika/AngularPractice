0316dc467f69baaaf9355062a881ee0f2379dc89
singhanamika <anasingh1593@gmail.com>
2018-03-28 01:16:51 -0400

    Update usertable.component.ts

--- src/app/usertable/usertable.component.ts
+++ src/app/usertable/usertable.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {UserService} from 'app/user.service';
-import { Observable } from 'rxjs/Observable';
-import { DataSource } from '@angular/cdk/collections';
-import { User} from 'app/user.model';
+import { Component, OnInit, Input } from '@angular/core';
+//import {UserService} from 'app/user.service';
+//import { Observable } from 'rxjs/Observable';
+//import { DataSource } from '@angular/cdk/collections';
+//import { User} from 'app/user.model';
 @Component({
   selector: 'app-usertable',
   templateUrl: './usertable.component.html',
   styleUrls: ['./usertable.component.css']
 })
 export class UsertableComponent implements OnInit {
-displayedColumns = ['UserId','id','title'];
-  dataSource = new UserDataSource(this.userService);
-  constructor(private userService: UserService) { }
+//displayedColumns = ['UserId','id','title'];
+  //dataSource = new UserDataSource(this.userService);
+  public arrayOfKeys;
+  @Input() course: Array<Object>=[];
+  constructor() { this.arrayOfKeys = Object.keys(this.course);}
 
   ngOnInit() {
   }
 
-}
-export class UserDataSource extends DataSource<any> {
-	constructor(private userService: UserService){super();}
-	
-		
-	
-	
-	connect(): Observable<User[]>{
-		return this.userService.getUser();
-	}
-	disconnect(){}
 }

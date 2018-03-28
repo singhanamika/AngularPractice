7bb6b4e622d82017c34a83ddb03b274abf23edfa
singhanamika <anasingh1593@gmail.com>
2018-03-28 01:20:45 -0400

    Create keyspipe.pipe.spec.ts

---
+++ src/app/keyspipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { KeyspipePipe } from './keyspipe.pipe';
+
+describe('KeyspipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new KeyspipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
